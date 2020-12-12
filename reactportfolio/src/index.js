5a5bcfe1173a14fdb3203f744af69b832da98db6
Natalie Rodriguez <natalierodri96@gmail.com>
2020-12-12 11:10:57 -0600

    index.js broswer router

--- reactportfolio/src/index.js
+++ reactportfolio/src/index.js
@@ -5,13 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </BrowserRouter>
+  , document.getElementById('root'));
+  registerServiceWorker();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
