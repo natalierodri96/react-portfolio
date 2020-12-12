2a20471de5ff66c979b2ebe2956f52406876bdaa
Natalie Rodriguez <natalierodri96@gmail.com>
2020-12-12 13:46:25 -0600

    app js

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './components/main';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
@@ -9,18 +10,18 @@ function App() {
     <Layout style={{background: 'url(https://wallpapersdsc.net/wp-content/uploads/2016/09/Orange-Flower-Wallpapers-HD.jpg) center / cover'}}>
         <Header transparent title="Title" style={{color: 'white'}}>
             <Navigation>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="contact">Contact</Link>
             </Navigation>
         </Header>
         <Drawer title="Title">
             <Navigation>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="contact">Contact</Link>
             </Navigation>
         </Drawer>
         <div className="page-content"/>
--- reactportfolio/src/components/projects.js
+++ reactportfolio/src/components/projects.js
@@ -2,7 +2,7 @@ import React, { Component} from 'react';
 class Projects extends Component {
 render() {
   return (
-    <div><h1>Projects Page</h1></div>
+    <div><h1>Projects</h1></div>
   )
 }
 }
