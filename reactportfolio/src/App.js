181124a5347f35380a4195e4bc555230f91bb934
Natalie Rodriguez <natalierodri96@gmail.com>
2020-12-11 21:18:57 -0600

    new folders and js files

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 
 function App() {
   return (
-    <div> 
-      <h1>Hello World!</h1>
-    </div>
+    <div style={{height: '300px', position: 'relative'}}>
+    <Layout style={{background: 'url(https://wallpapersdsc.net/wp-content/uploads/2016/09/Orange-Flower-Wallpapers-HD.jpg) center / cover'}}>
+        <Header transparent title="Title" style={{color: 'white'}}>
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content />
+    </Layout>
+</div>
   );
 }
 
