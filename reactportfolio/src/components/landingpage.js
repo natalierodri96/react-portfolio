00de97f4ea0f03802ac61826e07ddc1d62006a5a
Natalie Rodriguez <natalierodri96@gmail.com>
2020-12-12 22:36:30 -0600

    changes to app.css

--- reactportfolio/src/components/landingpage.js
+++ reactportfolio/src/components/landingpage.js
@@ -10,7 +10,7 @@ render() {
     <Grid className="landing-grid">
     <Cell col={12}>
     <img
-      src="https://mail.google.com/mail/u/0/?ui=2&view=btop&ver=ops2cvpehp6#attid%253Datt_1765a23b144ab569_0.1_b4d810d1_413d5eaf_94d70dce_98993f12_3b7397fb%25252FImage-1.png"
+      src="https://img0-placeit-net.s3-accelerate.amazonaws.com/uploads/stage/stage_image/64998/optimized_large_thumb_stage.jpg"
       alt="avatar"
       className="avatar-img"/>
       <div className="banner-text">
