724ea50eb6687b1d5b3920bc173ed3c2e3bb12cf
Natalie Rodriguez <natalierodri96@gmail.com>
2020-12-13 19:33:50 -0600

    project.js

--- reactportfolio/src/components/projects.js
+++ reactportfolio/src/components/projects.js
@@ -1,8 +1,18 @@
 import React, { Component} from 'react';
+
+
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
 render() {
   return (
-    <div><h1>Projects</h1></div>
+    <div className="category-tab">
+      <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId})} ripple>
+
+      </Tabs>
+    </div>
   )
 }
 }
