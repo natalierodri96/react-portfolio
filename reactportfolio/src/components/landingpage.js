8c9e4d74cbda702c88947ace86578572880514f2
Natalie Rodriguez <natalierodri96@gmail.com>
2020-12-13 18:23:57 -0600

    landing page

--- reactportfolio/src/components/landingpage.js
+++ reactportfolio/src/components/landingpage.js
@@ -12,9 +12,21 @@ render() {
     <img
       src="https://img0-placeit-net.s3-accelerate.amazonaws.com/uploads/stage/stage_image/64998/optimized_large_thumb_stage.jpg"
       alt="avatar"
-      className="avatar-img"/>
+      className="avatar-img"
+      />
+
       <div className="banner-text">
         <h1>Full Stack Web Developer</h1>
+        <hr/>
+        <p>HTML | CSS | Bootstrap | Javascript | React | NodeJS | Express | MongoDB |</p>
+      <div className="social">
+        {/*LinkedIn*/}
+        <a href="https://www.linkedin.com/in/natalie-rodriguez-65b011132/" target="_blank">
+        </a>
+         {/*GitHub*/}
+         <a href="https://github.com/natalierodri96/react-portfolio" target="_blank">
+        </a>
+      </div>
       </div>
     </Cell>
     </Grid>
